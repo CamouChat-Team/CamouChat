406acd4cda5206e935604b7333e12631449aba2b
BITS-Rohit <rohitguptaradheradhe123@gmail.com>
2026-03-18 16:01:30 +0530

    chore(test): smoke_test.py added at tests/ to quickly run and check if codebase works or not.

---
+++ tests/smoke_test.py
@@ -0,0 +1,129 @@
+"""
+Smoke Test File to test every module before pushing to release.
+"""
+import asyncio
+from asyncio import Queue
+from typing import List
+
+from camouchat.BrowserManager import (
+    BrowserConfig,
+    Platform,
+    BrowserForgeCompatible,
+    ProfileManager,
+    CamoufoxBrowser,
+    StorageType,
+)
+from camouchat.Filter import MessageFilter
+from camouchat.StorageDB import SQLAlchemyStorage
+from camouchat.WhatsApp import (
+    Login,
+    WebSelectorConfig,
+    ChatProcessor, MessageProcessor,
+)
+from camouchat.WhatsApp.models import Chat, Message
+from camouchat.camouchat_logger import camouchatLogger
+
+
+async def main():
+    """The main function to run."""
+    # 1. Profile
+    pm = ProfileManager()
+    profile = pm.create_profile(
+        platform=Platform.WHATSAPP,
+        profile_id="Work",
+        storage_type=StorageType.SQLITE
+    )
+
+    # 2. Browser Config
+    browser_forge = BrowserForgeCompatible()
+
+    config = BrowserConfig.from_dict({
+        "platform": Platform.WHATSAPP,
+        "locale": "en-US",
+        "enable_cache": False,
+        "headless": False,
+        "fingerprint_obj": browser_forge,
+    })
+
+    # 3. Browser
+    browser = CamoufoxBrowser(
+        config=config,
+        profile=profile,
+        log=camouchatLogger,
+    )
+
+    page = await browser.get_page()
+
+    # 4. UI Config
+    ui = WebSelectorConfig(page=page, log=camouchatLogger)
+
+    # 5. Login (QR first time)
+    login = Login(page=page, UIConfig=ui, log=camouchatLogger)
+
+    await login.login(
+        method=0  # [QR] , Can check by giving 1 and then 2 more params : number[int], country[str]
+    )
+
+    # 6. Chat Fetch
+    chat_processor = ChatProcessor(
+        page=page,
+        log=camouchatLogger,
+        ui_config=ui,
+    )
+    storage = SQLAlchemyStorage.from_profile(profile=profile, queue=Queue(), log=camouchatLogger)
+    M_filter = MessageFilter()  # Using Default setup , LimitTime = 3600, MaxMessagePerWindow = 10 , Window_Seconds = 60
+
+    print("Fetching chats...\n")
+    message_processor = MessageProcessor(
+        page=page,
+        log=camouchatLogger,
+        storage_obj=storage,
+        ui_config=ui,
+        chat_processor=chat_processor,
+        filter_obj=M_filter
+    )
+
+    from camouchat.WhatsApp import ReplyCapable
+    reply_obj = ReplyCapable(page=page, ui_config=ui)
+
+    from camouchat.WhatsApp import HumanInteractionController
+    op = HumanInteractionController(page=page, ui_config=ui)
+
+    processed = set()
+    print(f"Profile path = [{profile.profile_dir}]")
+    print(f"Database URL = [{profile.database_url}]")
+
+    async with storage:
+        for _ in range(50):
+            chats: List[Chat] = await chat_processor.fetch_chats(limit=4)  # Top 4 chats fetched
+            for chat in chats:
+                print(chat)
+                print("---Entering MessageProcessor...\n")
+
+                # Using only_new=True to skip processed history.
+                messages: List[Message] = await message_processor.fetch_messages(chat=chat, only_new=True)
+                for msg in messages:
+                    print(msg)
+                    print("---------")
+                    if msg.message_id not in processed:
+                        if msg.raw_data == "camouchat-hi":  # Reply Back
+                            do_rep = await reply_obj.reply(message=msg, humanize=op, text="Hi From CamouChat")
+                            if do_rep:
+                                print(f"Success ---- Replied for {msg.raw_data}  ********** ")
+                            else:
+                                print(f"Failed ❌❌❌❌❌❌❌  ---- Replied for {msg.raw_data} ❌❌❌❌❌❌❌❌  ")
+                        if msg.raw_data == "shut-down":
+                            do_rep = await reply_obj.reply(message=msg, humanize=op, text="Shutting Down ....")
+                            if do_rep:
+                                print(f"Success ---- Replied for {msg.raw_data}  ********** ")
+
+                                print(f"Profile path = [{profile.profile_dir}]")
+                                return
+                            else:
+                                print("Failed shutdown.")
+                        processed.add(msg.message_id)  # Saves
+            await asyncio.sleep(2)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
